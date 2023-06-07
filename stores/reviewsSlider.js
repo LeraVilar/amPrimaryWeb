a5797dccbbb1de9d3ef5451d2f35bb4eaab40955
Ruslan-Ragilo <65918169+Ruslan-Ragilo@users.noreply.github.com>
2023-06-07 16:54:57 +0300

    Update reviewsSlider.js

--- stores/reviewsSlider.js
+++ stores/reviewsSlider.js
@@ -3,27 +3,19 @@ import { defineStore } from 'pinia'
 export const useReviewsSlider = defineStore('reviewsSlider', {
   state: () => ({
     dataReviewsSlider: [],
-    typeReviews: ''
   }),
   getters: {
     getReviewsSlider() {
-      if (!this.typeReviews) {
-        return this.dataReviewsSlider
-      } else {
-        return this.dataReviewsSlider.filter(el => el.typeReviews === this.typeReviews)
-      }
-      
+      return this.dataReviewsSlider
     }
   },
   actions: {
-    async fetchDataReviewsSlider() {
-      const res = await fetch(import.meta.env.VITE_URL_API + '/api/reviews-sliders');
+    async fetchDataReviewsSlider(value) {
+      const filterData = value ? `filters[typeReviews][$eq]=${value}&` : ''
+      // console.log(import.meta.env.VITE_URL_API + `/api/reviews-sliders?&${filterData}pagination[limit]=-1`);
+      const res = await fetch(import.meta.env.VITE_URL_API + `/api/reviews-sliders?&${filterData}pagination[limit]=-1`);
       const data = await res.json()
       this.dataReviewsSlider = data.data.map(el => el.attributes)
     },
-
-    filterReviews(type = '') {
-      this.typeReviews = type
-    }
   }
 })
