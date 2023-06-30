99f7772c675f6544729a574b1e7f9d2566d3419b
LeraVilar <69603826+LeraVilar@users.noreply.github.com>
2023-06-30 23:48:21 +0400

    footerFix

--- components/theFooter.vue
+++ components/theFooter.vue
@@ -71,15 +71,15 @@
       </div>
     </div>
     <div class="academy-footer__bottom">
-      <a style="color: #644c5c; text-decoration: none; width: 100%;" :href="link + '/uploads/PUBLI_Ch_NA_Ya_OFERTA_0d52dc1276.pdf'" target="_blank" class="academy-footer__bottom-column">
+      <a style="color: #644c5c; text-decoration: none; width: 100%;" :href="link + '/uploads/PUBLI_Ch_NA_Ya_OFERTA_db374e4233.pdf'" target="_blank" class="academy-footer__bottom-column">
         Публичная оферта
       </a>
-      <a style="color: #644c5c; text-decoration: none; width: 100%;" :href="link + '/uploads/Politika_obrabotki_personalnyh_dannyh_IP_Dzhobava_E_M_7e09d3b087.pdf'" target="_blank" class="academy-footer__bottom-column">
+      <a style="color: #644c5c; text-decoration: none; width: 100%;" :href="link + '/uploads/Politika_obrabotki_personalnyh_dannyh_IP_Dzhobava_E_M_42ef36e3d9.pdf'" target="_blank" class="academy-footer__bottom-column">
         Политика персональных данных
       </a>
-      <div style="width: 100%;" class="academy-footer__bottom-column">
+      <a href="https://lsd-agency.ru" style="width: 100%;" class="academy-footer__bottom-column">
         Разработка сайта
-      </div>
+      </a>
     </div>
   </footer>
 </template>
