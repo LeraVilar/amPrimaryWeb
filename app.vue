ce32aa13983ca49b554ac9a282b6af3d8a54b9e1
arsenilamaka <arsenilamakaniebieski@gmail.com>
2026-02-20 09:09:28 +0100

    add vk pixel

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup>
+useHead({
+  noscript: [
+    {
+      children:
+        '<div><img src="https://top-fwz1.mail.ru/counter?id=3743560;js=na" style="position:absolute;left:-9999px;" alt="Top.Mail.Ru" /></div>',
+    },
+  ],
+});
+</script>
+
 <template>
   <the-header />
   <main class="page-container">
@@ -7,53 +18,53 @@
 </template>
 
 <style lang="scss">
-  html {
-    scroll-behavior: smooth;
+html {
+  scroll-behavior: smooth;
+}
+
+.page-container {
+  overflow: hidden;
+  padding: 90px 0 150px 0;
+
+  @media screen and (max-width: 1200px) {
+    padding: 50px 0 120px 0;
   }
 
-  .page-container {
-    overflow: hidden;
-    padding: 90px 0 150px 0;
+  @media screen and (max-width: 680px) {
+    padding: 50px 0 90px 0;
+  }
+}
 
-    @media screen and (max-width: 1200px) {
-      padding: 50px 0 120px 0;
-    }
+.section-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 1240px;
+  margin: 0 auto;
+  padding: 0 40px;
 
-    @media screen and (max-width: 680px) {
-      padding: 50px 0 90px 0;
-    }
+  @media screen and (max-width: 1200px) {
+    max-width: 680px;
   }
 
-  .section-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: 1240px;
-    margin: 0 auto;
-    padding: 0 40px;
+  @media screen and (max-width: 680px) {
+    padding: 0 20px;
+  }
+}
 
-    @media screen and (max-width: 1200px) {
-      max-width: 680px;
-    }
+.section-block {
+  margin-top: 150px;
 
-    @media screen and (max-width: 680px) {
-      padding: 0 20px;
-    }
+  @media screen and (max-width: 1200px) {
+    margin-top: 120px;
   }
 
-  .section-block {
-    margin-top: 150px;
-
-    @media screen and (max-width: 1200px) {
-      margin-top: 120px;
-    }
-
-    @media screen and (max-width: 680px) {
-      margin-top: 90px;
-    }
+  @media screen and (max-width: 680px) {
+    margin-top: 90px;
   }
+}
 
-  .background-violet {
-    background-color: #644C5C;
-  }
+.background-violet {
+  background-color: #644c5c;
+}
 </style>
